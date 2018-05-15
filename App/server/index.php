97298000cee77b9fad47e4e5fd36288947aa7fdb
Carlos R. Zúñiga <carlosrozuma@gmail.com>
2018-05-14 17:48:32 -0700

    Added basic Routes

--- App/server/index.php
+++ App/server/index.php
@@ -26,39 +26,74 @@ $app->get('/', function(Request $request, Response $response, $params){
 //--------------------------
 //  USER ROUTES
 //--------------------------
-//$app->get('/users', 'UserController:getUsers');
+$app->get('/users', 'UserController:getUsers');
+$app->get('/users/{id}', 'UserController:getUser_ById');
+$app->post('/users', 'UserController:createUser');
+$app->put('/users', 'UserController:updateUser');
+$app->delete('/users', 'UserController:deleteUser');
 
 //--------------------------
 //  STUDENT ROUTES
 //--------------------------
+$app->get('/students', 'StudentController:getStudents');
+$app->get('/students/{id}', 'StudentController:getStudent_ById');
+$app->post('/students', 'StudentController:createStudent');
+$app->put('/students', 'StudentController:updateStudent');
+$app->delete('/students', 'StudentController:deleteStudent');
 
 //--------------------------
 //  CAREER ROUTES
 //--------------------------
+$app->get('/careers', 'CareerController:getCareers');
+$app->get('/careers/{id}', 'CareerController:getCareer_ById');
+$app->post('/careers', 'CareerController:createCareer');
+$app->put('/careers', 'CareerController:updateCareer');
+$app->delete('/careers', 'CareerController:deleteCareer');
 
 //--------------------------
 //  PLAN ROUTES
 //--------------------------
+$app->get('/plan', 'PlanController:getPlans');
+$app->get('/plan/{id}', 'PlanController:getPlan_ById');
+$app->post('/plan', 'PlanController:createPlan');
+$app->put('/plan', 'PlanController:updatePlan');
+$app->delete('/plan', 'PlanController:deletePlan');
 
 //--------------------------
 //  SUBJECT ROUTES
 //--------------------------
-
+$app->get('/subject', 'SubjectController:getSubjects');
+$app->get('/subject/{id}', 'SubjectController:getSubject_ById');
+$app->post('/subject', 'SubjectController:createSubject');
+$app->put('/subject', 'SubjectController:updateSubject');
+$app->delete('/subject', 'SubjectController:deleteSubject');
 
 //--------------------------
 //  PERIOD ROUTES
 //--------------------------
-
+$app->get('/period', 'PeriodController:getPeriods');
+$app->get('/period/{id}', 'PeriodController:getPeriod_ById');
+$app->post('/period', 'PeriodController:createPeriod');
+$app->put('/period', 'PeriodController:updatePeriod');
+$app->delete('/period', 'PeriodController:deletePeriod');
 
 //--------------------------
 //  SCHEDULE ROUTES
 //--------------------------
-
+$app->get('/schedule', 'ScheduleController:getSchedules');
+$app->get('/schedule/{id}', 'ScheduleController:getSchedule_ById');
+$app->post('/schedule', 'ScheduleController:createSchedule');
+$app->put('/schedule', 'ScheduleController:updateSchedule');
+$app->delete('/schedule', 'ScheduleController:deleteSchedule');
 
 //--------------------------
 //  ADVISORY ROUTES
 //--------------------------
-
+$app->get('/advisory', 'AdvisoryController:getAdvisorys');
+$app->get('/advisory/{id}', 'AdvisoryController:getAdvisory_ById');
+$app->post('/advisory', 'AdvisoryController:createAdvisory');
+$app->put('/advisory', 'AdvisoryController:updateAdvisory');
+$app->delete('/advisory', 'AdvisoryController:deleteAdvisory');
 
 
 
