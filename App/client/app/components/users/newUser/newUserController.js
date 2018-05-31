eb43f5bb4ab0314c339180c3546cafaa6be15e65
Carlos R. Zúñiga <carlosrozuma@gmail.com>
2018-05-31 14:32:21 -0700

    New User register

--- App/client/app/components/users/newUser/newUserController.js
+++ App/client/app/components/users/newUser/newUserController.js
@@ -1,23 +1,68 @@
-app.controller('NewUserController', function($scope, $http, Notification, UsersService){
+app.controller('NewUserController', function($scope, $http, $timeout, NewUserService, Notification){
     $scope.page.title = "Usuarios > Nuevo";
+    $scope.loading = false;
+    // $scope.error = {
+    //     status: false,
+    //     message: ""
+    // };
+    $scope.success = false;
+
+    /**
+     * 
+     * @param {*} user 
+     * @return bool
+     */
+    var validate = function(user){
+        if( user.pass != user.pass2 ){
+            Notification.warning('Contraseñas no coinciden');
+            return false;
+        }
+            
+        return true;
+    }
 
     /**
      * 
      * @param {*} user 
      */
     $scope.addUser = function(user){
+        
+        if( !validate(user) )
+            return;
+
         //Se pone en cargando
-        $scope.newUser.status = true
+        $scope.loading = true;
+
+        NewUserService.addUser(user,
+            function(success){
+                
+            },
+            function (error){
+                
+            });
 
         //Se hace peticion
-        UsersService.addUser(user, 
-            function(success){
-                Notification.success('Datos obtenidos');
-                $scope.getUsers();
-            }, 
-            function(error){
-                Notification.error("Error al registrar usuario: "+error.data.message);
-            });
+        // NewUserService.addUser(user, 
+        //     function(success){
+        //         $scope.success = true;
+        //         $scope.loading = false;
+        //         $scope.getUsers();
+
+        //         $timeout(function(){
+        //             $scope.success = false;
+        //         },2000);
+        //     }, 
+        //     function(error){
+        //         $scope.loading = false;
+
+        //         $scope.error = {
+        //             status = true,
+        //             message: "asdad"
+        //         }
+        //         $timeout(function(){
+        //             $scope.error.status = false;
+        //         },2000);
+        //     });
     }
 
 });
