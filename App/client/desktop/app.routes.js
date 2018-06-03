bd01335d4aa55bac580388169a675554e9d59454
Carlos R. Zúñiga <carlosrozuma@gmail.com>
2018-06-03 02:37:35 -0700

    Added Desktop module for students

---
+++ App/client/desktop/app.routes.js
@@ -0,0 +1,9 @@
+app.config( function ($routeProvider) {
+    $routeProvider
+        
+        .when("/", {
+            controller: "HomeController",
+            templateUrl: "app/components/home/homeView.html"
+        })
+        .otherwise("/");
+});
---
+++ App/client/desktop/app/components/home/homeService.js
@@ -0,0 +1,15 @@
+app.service('HomeService', function($http){
+    
+
+    // this.getCareers = function(successCallback, errorCallback){
+    //     $http({
+    //         method: 'GET',
+    //         url: "http://asesoriaspar.ronintopics.com/index.php/careers"
+    //     }).then(function(success){
+    //         successCallback(success);
+    //     }, function(error){
+    //         errorCallback(error);
+    //     });
+    // } 
+
+});
